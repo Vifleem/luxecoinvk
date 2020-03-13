91f0ca73c8475b9ec18bb7373105f84ac7750559
Vifleem <sobole_80@mail.ru>
2020-03-13 08:59:19 +0600

    Updates

---
+++ precache-manifest.7db6460ca578efc04ce697b2973b964e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81681bd97ccf9620d56e8c92e099661f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ab24014e1de9642b087d",
+    "url": "./static/css/2.3e9c613c.chunk.css"
+  },
+  {
+    "revision": "d12e00d11f6bb7e820b4",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "ab24014e1de9642b087d",
+    "url": "./static/js/2.ccbe32f5.chunk.js"
+  },
+  {
+    "revision": "d12e00d11f6bb7e820b4",
+    "url": "./static/js/main.f6bd201b.chunk.js"
+  },
+  {
+    "revision": "02328ac8382bd6a6ca82",
+    "url": "./static/js/runtime-main.020021d2.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
